d20213026bd915c3d857c77f8fd8716b7de2ccb6
Paccy10 <pacifiqueclement@gmail.com>
2021-01-05 15:41:42 +0200

    Backend

---
+++ server/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const mongoose = require('mongoose');
+const schema = require('./schema');
+
+const app = express();
+
+mongoose.connect('mongodb://localhost:27017/graphql-tutorial');
+mongoose.connection.once('open', () => console.log('Database Connected...'));
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema,
+    graphiql: true
+  })
+);
+
+app.listen(5000, () => console.log('Server running on port 5000...'));
